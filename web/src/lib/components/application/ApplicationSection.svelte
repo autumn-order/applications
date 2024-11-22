9210e91272b33331ba1520a1d76ebe65f29616f4
Hyziri <hyziri@protonmail.com>
2024-11-22 16:59:54 -0600

    Improve application rejection page by adjusting format and including who reviewed the application.

--- web/src/lib/components/application/ApplicationSection.svelte
+++ web/src/lib/components/application/ApplicationSection.svelte
@@ -1,145 +1,144 @@
-
 <script lang="ts" context="module">
-    export enum ApplicationSteps {
-        SeAT = 0,
-        Discord = 1,
-        Questions = 2,
-        Review = 3,
-        AcceptInvite = 4,
-        Joined = 5
-    }
+	export enum ApplicationSteps {
+		SeAT = 0,
+		Discord = 1,
+		Questions = 2,
+		Review = 3,
+		AcceptInvite = 4,
+		Joined = 5
+	}
 </script>
 
 <script lang="ts">
-	import { ApplicationStatus, type ApplicationDto } from "$lib/model/application";
-	import type { UserDto } from "$lib/model/user";
-	import { getApplication } from "$lib/stores/application";
-	import { onMount } from "svelte";
-	import type { Unsubscriber } from "svelte/motion";
-	import { CORPORATIONS } from "../../../constants";
+	import { ApplicationStatus, type ApplicationDto } from '$lib/model/application';
+	import type { UserDto } from '$lib/model/user';
+	import { getApplication } from '$lib/stores/application';
+	import { onMount } from 'svelte';
+	import type { Unsubscriber } from 'svelte/motion';
+	import { CORPORATIONS } from '../../../constants';
 
-    import {
-        Step1ChooseLocation,
-        Step2SetupSeAT,
-        Step3SetupDiscord,
-        Step4AnswerQuestions,
-        Step5ApplicationReview,
-        Step6AcceptInvite,
-    } from './steps';
+	import {
+		Step1ChooseLocation,
+		Step2SetupSeAT,
+		Step3SetupDiscord,
+		Step4AnswerQuestions,
+		Step5ApplicationReview,
+		Step6AcceptInvite
+	} from './steps';
 
-	import AppRejected from "./results/AppRejected.svelte";
-	import AppProgressIndicator from "./AppProgressIndicator.svelte";
-	import Welcome from "./results/Welcome.svelte";
-	import MemberBanner from "./MemberBanner.svelte";
-	import SwitchLocationBanner from "./SwitchLocationBanner.svelte";
-    
-    let viewed_step: ApplicationSteps | undefined = undefined;
-    let current_step: ApplicationSteps = 0;
+	import AppRejected from './results/AppRejected.svelte';
+	import AppProgressIndicator from './AppProgressIndicator.svelte';
+	import Welcome from './results/Welcome.svelte';
+	import MemberBanner from './MemberBanner.svelte';
+	import SwitchLocationBanner from './SwitchLocationBanner.svelte';
 
-    let user: UserDto | null | undefined = $$props.user;
-    let application: undefined | null | ApplicationDto = undefined;
+	let viewed_step: ApplicationSteps | undefined = undefined;
+	let current_step: ApplicationSteps = 0;
 
-    onMount(() => {
-        let applicationUnsubscribe: Unsubscriber | null = null;
+	let user: UserDto | null | undefined = $$props.user;
+	let application: undefined | null | ApplicationDto = undefined;
 
-        if (user) {
-            const applicationStore = getApplication();
+	onMount(() => {
+		let applicationUnsubscribe: Unsubscriber | null = null;
 
-            applicationUnsubscribe = applicationStore.subscribe(value => {
-                application = value; 
+		if (user) {
+			const applicationStore = getApplication();
 
-                if (CORPORATIONS.some(corp => corp.corporation_id === user?.corporation_id)) {
-                    application = null;
-                    current_step = ApplicationSteps.Joined;
-                    
-                    if (viewed_step === undefined) {
-                        viewed_step = ApplicationSteps.AcceptInvite;
-                    }
-                } else if (application) {
-                    if (!application.seat_completed) {
-                        current_step = ApplicationSteps.SeAT;
-                    } else if (!application.discord_completed) {
-                        current_step = ApplicationSteps.Discord;
-                    } else if (
-                        application.questions.referrer === null ||
-                        application.questions.region === null ||
-                        application.questions.languages.length < 1 ||
-                        application.questions.interests.length < 1 ||
-                        application.questions.end_goals.length === 0 ||
-                        application.questions.why_autumn.length === 0
-                    ) {
-                        current_step = ApplicationSteps.Questions;
-                    } else if (application.status === ApplicationStatus.Pending) {
-                        current_step = ApplicationSteps.Review;
-                    } else if (application.status === ApplicationStatus.Accepted) {
-                        current_step = ApplicationSteps.AcceptInvite;
-                    }
+			applicationUnsubscribe = applicationStore.subscribe((value) => {
+				application = value;
 
-                    if (viewed_step === undefined) {
-                        viewed_step = current_step;
-                    }
-                }
-            });
-        }
+				if (CORPORATIONS.some((corp) => corp.corporation_id === user?.corporation_id)) {
+					application = null;
+					current_step = ApplicationSteps.Joined;
 
-        return () => {
-            if (applicationUnsubscribe) {
-                applicationUnsubscribe();
-            }
-        }
-    });
+					if (viewed_step === undefined) {
+						viewed_step = ApplicationSteps.AcceptInvite;
+					}
+				} else if (application) {
+					if (!application.seat_completed) {
+						current_step = ApplicationSteps.SeAT;
+					} else if (!application.discord_completed) {
+						current_step = ApplicationSteps.Discord;
+					} else if (
+						application.questions.referrer === null ||
+						application.questions.region === null ||
+						application.questions.languages.length < 1 ||
+						application.questions.interests.length < 1 ||
+						application.questions.end_goals.length === 0 ||
+						application.questions.why_autumn.length === 0
+					) {
+						current_step = ApplicationSteps.Questions;
+					} else if (application.status === ApplicationStatus.Pending) {
+						current_step = ApplicationSteps.Review;
+					} else if (application.status === ApplicationStatus.Accepted) {
+						current_step = ApplicationSteps.AcceptInvite;
+					}
 
-    function setViewedStep(new_viewed_step: ApplicationSteps) {
-        if (new_viewed_step > current_step) {
-            return;
-        }
+					if (viewed_step === undefined) {
+						viewed_step = current_step;
+					}
+				}
+			});
+		}
 
-        viewed_step = new_viewed_step;
-        window.scrollTo(0, 0);
-    }
+		return () => {
+			if (applicationUnsubscribe) {
+				applicationUnsubscribe();
+			}
+		};
+	});
 
-    function setCurrentStep(new_current_step: ApplicationSteps) {
-        current_step = new_current_step;
-    }
+	function setViewedStep(new_viewed_step: ApplicationSteps) {
+		if (new_viewed_step > current_step) {
+			return;
+		}
 
-    // BACKEND Logic
-    // 1. SeAT - check SeAT dev api is logged in user character is on SeAT
-    // 2. Discord - check SeAT dev connnector api if user has linked discord
-    // 3. Questions - check if user has filled out questions
-    // 4. Fetch application status from backend
-    // 5. This only works if affiliation is marked as one of the member corps which has upwards of an hour long delay due to ESI caching
+		viewed_step = new_viewed_step;
+		window.scrollTo(0, 0);
+	}
+
+	function setCurrentStep(new_current_step: ApplicationSteps) {
+		current_step = new_current_step;
+	}
+
+	// BACKEND Logic
+	// 1. SeAT - check SeAT dev api is logged in user character is on SeAT
+	// 2. Discord - check SeAT dev connnector api if user has linked discord
+	// 3. Questions - check if user has filled out questions
+	// 4. Fetch application status from backend
+	// 5. This only works if affiliation is marked as one of the member corps which has upwards of an hour long delay due to ESI caching
 </script>
 
 <div class="flex flex-col flex-grow w-full h-full">
-    {#if application === undefined}
-        <div>Loading...</div>
-    {:else if application === null && current_step !== ApplicationSteps.Joined}
-        <Step1ChooseLocation/>
-    {:else if application && application.status === ApplicationStatus.Rejected}
-        {#if application.status === ApplicationStatus.Rejected}
-            <AppRejected application={application}/>
-        {/if}
-    {:else if application || current_step === ApplicationSteps.Joined}
-        <AppProgressIndicator viewed_step={viewed_step} completed_steps={current_step} setViewedStep={setViewedStep}/>
-        {#if application}
-            <SwitchLocationBanner user={user}/>
-        {:else}
-            <MemberBanner/>
-        {/if}
-        {#if viewed_step === ApplicationSteps.SeAT}
-            <Step2SetupSeAT application={application} setViewedStep={setViewedStep}/>
-        {:else if viewed_step === ApplicationSteps.Discord}
-            <Step3SetupDiscord application={application} setViewedStep={setViewedStep}/>
-        {:else if viewed_step === ApplicationSteps.Questions}
-            <Step4AnswerQuestions application={application} setViewedStep={setViewedStep} setCurrentStep={setCurrentStep}/>
-        {:else if viewed_step === ApplicationSteps.Review}
-            <Step5ApplicationReview user={user} setViewedStep={setViewedStep}/>
-        {:else if viewed_step === ApplicationSteps.AcceptInvite || current_step === ApplicationSteps.Joined}
-            {#if current_step !== ApplicationSteps.Joined} 
-                <Step6AcceptInvite/>
-            {:else}
-                <Welcome user={user}/>
-            {/if}
-        {/if}
-    {/if}
+	{#if application === undefined}
+		<div>Loading...</div>
+	{:else if application === null && current_step !== ApplicationSteps.Joined}
+		<Step1ChooseLocation />
+	{:else if application && application.status === ApplicationStatus.Rejected}
+		{#if application.status === ApplicationStatus.Rejected}
+			<AppRejected {application} {user} />
+		{/if}
+	{:else if application || current_step === ApplicationSteps.Joined}
+		<AppProgressIndicator {viewed_step} completed_steps={current_step} {setViewedStep} />
+		{#if application}
+			<SwitchLocationBanner {user} />
+		{:else}
+			<MemberBanner />
+		{/if}
+		{#if viewed_step === ApplicationSteps.SeAT}
+			<Step2SetupSeAT {application} {setViewedStep} />
+		{:else if viewed_step === ApplicationSteps.Discord}
+			<Step3SetupDiscord {application} {setViewedStep} />
+		{:else if viewed_step === ApplicationSteps.Questions}
+			<Step4AnswerQuestions {application} {setViewedStep} {setCurrentStep} />
+		{:else if viewed_step === ApplicationSteps.Review}
+			<Step5ApplicationReview {user} {setViewedStep} />
+		{:else if viewed_step === ApplicationSteps.AcceptInvite || current_step === ApplicationSteps.Joined}
+			{#if current_step !== ApplicationSteps.Joined}
+				<Step6AcceptInvite />
+			{:else}
+				<Welcome {user} />
+			{/if}
+		{/if}
+	{/if}
 </div>
--- web/src/lib/components/application/results/AppRejected.svelte
+++ web/src/lib/components/application/results/AppRejected.svelte
@@ -6,6 +6,9 @@
 	import type { ApplicationDto } from '$lib/model/application';
 
 	let application: ApplicationDto = $$props.application;
+	let user = $$props.user;
+
+	let user_name = '';
 
 	let rejection_reason: string =
 		'No reason provided, please contact an officer for more information.';
@@ -15,17 +18,44 @@
 			rejection_reason = application.rejection_reason;
 		}
 	}
+
+	if (user) {
+		user_name = user.character_name;
+	}
 </script>
 
 <div class="flex flex-col gap-2 justify-center items-center h-full w-full text-center p-2 lg:p-6">
-	<h1 class="text-gradient text-xl sm:text-3xl font-bold w-full text-center">We're Sorry</h1>
+	<h1 class="text-gradient text-xl sm:text-3xl font-bold w-full text-center">
+		We're Sorry {user_name}
+	</h1>
 	<p>Unfortunately your application has been rejected for the following reason:</p>
-	<p class="border-color p-0.5 px-3 bg-red-800 min-w-32 rounded border text-center min-h-6">
-		{rejection_reason}
-	</p>
+	<div class="flex flex-col">
+		<p class="text-red-500 py-4 pb-6">
+			{rejection_reason}
+		</p>
+		{#if application.reviewer}
+			<div>
+				<p>Application Reviewed By</p>
+				<div class="py-4">
+					<a
+						class="flex gap-4 items-center hover:invert-[0.075] dark:hover:invert-[0.025]"
+						href="https://zkillboard.com/character/{application.reviewer.character_id}/"
+					>
+						<img
+							class="rounded-full w-16 h-16"
+							src="https://images.evetech.net/characters/{application.reviewer
+								.character_id}/portrait?size=64"
+							alt=""
+						/>
+						<p>{application.reviewer.character_name}</p>
+					</a>
+				</div>
+			</div>
+		{/if}
+	</div>
 	<p>So long as you aren't blacklisted you are more than welcome to apply again after 30 days.</p>
 	<p>If you believe this was a mistake please contact us via the Autumn Discord.</p>
-	<div>
+	<div class="pt-4">
 		<a href={DISCORD_LINK} target="_blank">
 			<Button class="flex gap-2" variant="outline">
 				<Fa icon={faDiscord} size="lg" />
